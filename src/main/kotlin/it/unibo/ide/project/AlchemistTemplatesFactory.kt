cf7de93269fad7bbbf0f851c47766809be63ba81
William Danese <william.danese@studio.unibo.it>
2019-06-11 00:12:51 +0200

    Add templates group name

--- src/main/kotlin/it/unibo/ide/project/AlchemistTemplatesFactory.kt
+++ src/main/kotlin/it/unibo/ide/project/AlchemistTemplatesFactory.kt
@@ -6,7 +6,11 @@ import com.intellij.platform.ProjectTemplatesFactory
 
 class AlchemistTemplatesFactory : ProjectTemplatesFactory() {
 
+    companion object {
+        private const val ALCHEMIST_GROUP = "Alchemist"
+    }
+
     override fun createTemplates(group: String?, context: WizardContext): Array<ProjectTemplate> = TODO()
 
-    override fun getGroups(): Array<String> = TODO()
+    override fun getGroups(): Array<String> = arrayOf(ALCHEMIST_GROUP)
 }
