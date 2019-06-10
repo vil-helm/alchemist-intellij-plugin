226b87f5523e107cff8c5a03dc5135180623a2f0
William Danese <william.danese@studio.unibo.it>
2019-06-11 01:02:02 +0200

    Add templates creation

--- src/main/kotlin/it/unibo/ide/project/AlchemistTemplatesFactory.kt
+++ src/main/kotlin/it/unibo/ide/project/AlchemistTemplatesFactory.kt
@@ -1,17 +1,25 @@
 package it.unibo.ide.project
 
+import com.intellij.ide.util.projectWizard.ModuleBuilder
 import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.platform.ProjectTemplate
 import com.intellij.platform.ProjectTemplatesFactory
+import com.intellij.platform.templates.BuilderBasedTemplate
 
 class AlchemistTemplatesFactory : ProjectTemplatesFactory() {
 
     companion object {
         private const val ALCHEMIST_GROUP = "Alchemist"
+
+        // Get template builders from the extensions of the <plugin.xml> file and convert them into templates.
+        private val TEMPLATES = ExtensionPointName.create<ModuleBuilder>("it.unibo.alchemist.templateBuilder")
+            .extensions.map { BuilderBasedTemplate(it) }
     }
 
     override fun createTemplates(group: String?, context: WizardContext): Array<ProjectTemplate> =
-        if (group in groups) TODO() else throw  IllegalArgumentException("The passed group is not correct for this templates factory")
+        if (group in groups) TEMPLATES.toTypedArray() else throw  IllegalArgumentException("The passed group is not correct for this templates factory")
 
     override fun getGroups(): Array<String> = arrayOf(ALCHEMIST_GROUP)
+
 }
