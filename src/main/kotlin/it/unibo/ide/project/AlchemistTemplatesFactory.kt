985b226092ac6423e21158ad91db3078de374df5
William Danese <william.danese@studio.unibo.it>
2019-06-11 00:09:35 +0200

    Initialize templates launcher

---
+++ src/main/kotlin/it/unibo/ide/project/AlchemistTemplatesFactory.kt
@@ -0,0 +1,12 @@
+package it.unibo.ide.project
+
+import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.platform.ProjectTemplate
+import com.intellij.platform.ProjectTemplatesFactory
+
+class AlchemistTemplatesFactory : ProjectTemplatesFactory() {
+
+    override fun createTemplates(group: String?, context: WizardContext): Array<ProjectTemplate> = TODO()
+
+    override fun getGroups(): Array<String> = TODO()
+}
