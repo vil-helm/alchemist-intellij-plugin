0355eb9a48ab9282df480bd82c66576c6f357e59
William Danese <william.danese@studio.unibo.it>
2019-06-11 11:25:38 +0200

    Initialize copying behaviour of the template

--- src/main/kotlin/it/unibo/ide/project/TemplateGradleModuleBuilder.kt
+++ src/main/kotlin/it/unibo/ide/project/TemplateGradleModuleBuilder.kt
@@ -2,6 +2,7 @@ package it.unibo.ide.project
 
 import com.intellij.ide.util.projectWizard.ModuleWizardStep
 import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider
 import org.jetbrains.plugins.gradle.service.project.wizard.GradleModuleBuilder
 import javax.swing.Icon
@@ -21,5 +22,16 @@ abstract class TemplateGradleModuleBuilder : GradleModuleBuilder() {
         wizardContext: WizardContext,
         modulesProvider: ModulesProvider
     ): Array<ModuleWizardStep> = super.createWizardSteps(wizardContext, modulesProvider).copyOfRange(1, 2)
+
+    // This override copies the template files in the new module.
+    override fun setupModule(module: Module) = super.setupModule(module).apply {
+        // Copy the template files.
+        copyTemplateFilesTo(contentEntryPath ?: return)
+    }
+
+    // This function copies the template files to the created root directory.
+    private fun copyTemplateFilesTo(rootDirectoryPath: String) {
+        TODO()
+    }
 
 }
