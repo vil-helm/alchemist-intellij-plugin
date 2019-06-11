fc1f0d0e0ad839ea375f2d5a35d2138939ae101d
William Danese <william.danese@studio.unibo.it>
2019-06-11 11:12:27 +0200

    Configure template data

--- src/main/kotlin/it/unibo/ide/project/TemplateGradleModuleBuilder.kt
+++ src/main/kotlin/it/unibo/ide/project/TemplateGradleModuleBuilder.kt
@@ -1,5 +1,16 @@
 package it.unibo.ide.project
 
 import org.jetbrains.plugins.gradle.service.project.wizard.GradleModuleBuilder
+import javax.swing.Icon
 
-abstract class TemplateGradleModuleBuilder : GradleModuleBuilder()
+abstract class TemplateGradleModuleBuilder : GradleModuleBuilder() {
+
+    abstract override fun getBuilderId(): String
+
+    abstract override fun getPresentableName(): String
+
+    abstract override fun getDescription(): String
+
+    abstract override fun getNodeIcon(): Icon
+
+}
