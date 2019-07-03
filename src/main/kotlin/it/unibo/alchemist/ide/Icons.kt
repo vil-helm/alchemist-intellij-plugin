136d2dd4656407eda924006f5182a370e4cf7701
William Danese <william.danese@studio.unibo.it>
2019-07-03 16:27:30 +0200

    Add alchemist logo

---
+++ src/main/kotlin/it/unibo/alchemist/ide/Icons.kt
@@ -0,0 +1,11 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+// This class can be used in plugin.xml
+object Icons {
+
+    val ALCHEMIST_LOGO: Icon = IconLoader.getIcon("it/unibo/alchemist/icons/alchemist_logo.svg")
+
+}
