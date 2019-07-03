871717bcd7da58f1d1b5e5453119a79b6ff359cc
William Danese <william.danese@studio.unibo.it>
2019-07-03 16:17:48 +0200

    Remove the templates constant and create them directly in the appropriate function

--- src/main/kotlin/it/unibo/alchemist/ide/project/AlchemistTemplatesFactory.kt
+++ src/main/kotlin/it/unibo/alchemist/ide/project/AlchemistTemplatesFactory.kt
@@ -4,29 +4,30 @@ import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.platform.ProjectTemplate
 import com.intellij.platform.ProjectTemplatesFactory
 import com.intellij.platform.templates.BuilderBasedTemplate
+import icons.Icons
 import org.reflections.Reflections
 import org.reflections.scanners.ResourcesScanner
 import java.util.regex.Pattern
+import javax.swing.Icon
 
 class AlchemistTemplatesFactory : ProjectTemplatesFactory() {
 
     companion object {
+
         private const val ALCHEMIST_GROUP = "Alchemist"
 
         private const val TEMPLATES_FOLDER = "it/unibo/alchemist/templates"
 
-        // Get the templates builders from the resources.
-        private val TEMPLATES =
-            // Get all the resources from the templates directory.
-            Reflections(TEMPLATES_FOLDER, ResourcesScanner()).getResources(Pattern.compile(".*")).map {
-                // Get sub-folders of templates directory
-                it.substring(0, it.indexOf("/", TEMPLATES_FOLDER.length + 1))
-                // Get the builders from those sub-folders.
-            }.toSet().map { BuilderBasedTemplate(TemplateGradleModuleBuilder(it)) }
     }
 
+    // Get the templates from the resources.
     override fun createTemplates(group: String?, context: WizardContext): Array<ProjectTemplate> =
-        if (group in groups) TEMPLATES.toTypedArray() else throw IllegalArgumentException("The passed group is not correct for this templates factory")
+        // Get all the resources from the templates directory.
+        Reflections(TEMPLATES_FOLDER, ResourcesScanner()).getResources(Pattern.compile(".*")).map {
+            // Get sub-folders of templates directory
+            it.substring(0, it.indexOf("/", TEMPLATES_FOLDER.length + 1))
+            // Get the builders from those sub-folders anc create the templates.
+        }.toSet().map { BuilderBasedTemplate(TemplateGradleModuleBuilder(it)) }.toTypedArray()
 
     override fun getGroups(): Array<String> = arrayOf(ALCHEMIST_GROUP)
 
