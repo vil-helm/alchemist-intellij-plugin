8e6a6b8576987553af3bea6fe772265519b5eb27
William Danese <william.danese@studio.unibo.it>
2019-09-25 10:57:12 +0200

    Add a ktlint Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.31"
-    //id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
-    //id("org.jetbrains.dokka") version "0.9.18"
+    id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.jetbrains.intellij") version "0.4.9"
     id("com.gradle.build-scan") version "2.4.2"
-    //id("de.fayard.buildSrcVersions") version "0.6.1"
     id("org.danilopianini.git-sensitive-semantic-versioning") version "0.2.1"
 }
 
