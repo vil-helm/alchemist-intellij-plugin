23272296cf914a4dedb2ec69ccb6b79257c609e4
William Danese <william.danese@studio.unibo.it>
2019-09-25 10:50:23 +0200

    Add a Gradle plugin for Git-sensitive Semantic Versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.31"
+    //id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
+    //id("org.jetbrains.dokka") version "0.9.18"
     id("org.jetbrains.intellij") version "0.4.9"
     id("com.gradle.build-scan") version "2.4.2"
+    //id("de.fayard.buildSrcVersions") version "0.6.1"
+    id("org.danilopianini.git-sensitive-semantic-versioning") version "0.2.1"
 }
 
 repositories {
@@ -21,6 +25,10 @@ intellij {
 
 buildScan {
 
+}
+
+gitSemVer {
+    version = computeGitSemVer() // THIS IS MANDATORY, AND MUST BE LAST IN BLOCK
 }
 
 tasks.withType<KotlinCompile> {
