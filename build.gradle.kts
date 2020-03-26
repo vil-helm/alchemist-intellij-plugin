823ac5f4afe9e378a3554ef1e4d0f176e5f0b419
Danilo Pianini <danilo.pianini@gmail.com>
2020-03-26 13:06:37 +0100

    Transition to refreshVersions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version Versions.kotlin_scripting_compiler_embeddable
+    kotlin("jvm")
     jacoco
-    id("org.jlleitschuh.gradle.ktlint") version Versions.org_jlleitschuh_gradle_ktlint_gradle_plugin
-    id("org.jetbrains.dokka") version Versions.org_jetbrains_dokka_gradle_plugin
-    id("org.jetbrains.intellij") version Versions.org_jetbrains_intellij_gradle_plugin
-    id("com.gradle.build-scan") version Versions.com_gradle_build_scan_gradle_plugin
-    id("de.fayard.buildSrcVersions") version Versions.de_fayard_buildsrcversions_gradle_plugin
-    id("org.danilopianini.git-sensitive-semantic-versioning") version Versions.org_danilopianini_git_sensitive_semantic_versioning_gradle_plugin
+    id("org.jlleitschuh.gradle.ktlint")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.intellij")
+    id("org.danilopianini.git-sensitive-semantic-versioning")
 }
 
 repositories {
@@ -16,18 +14,15 @@ repositories {
 }
 
 dependencies {
-    implementation(Libs.classgraph)
+    implementation("io.github.classgraph:classgraph:_")
 }
 
 intellij {
     setPlugins("java", "gradle", "gradle-java")
 }
 
-buildScan {
-}
-
 gitSemVer {
-    version = computeGitSemVer() // THIS IS MANDATORY, AND MUST BE LAST IN BLOCK
+    version = computeGitSemVer()
 }
 
 tasks.withType<KotlinCompile> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,21 @@
+import de.fayard.dependencies.bootstrapRefreshVersionsAndDependencies
+
 rootProject.name = "alchemist-intellij-plugin"
+
+buildscript {
+    repositories { gradlePluginPortal() }
+    dependencies.classpath("de.fayard:dependencies:0.5.7")
+}
+
+bootstrapRefreshVersionsAndDependencies()
+
+plugins {
+    id("com.gradle.enterprise") version "3.2"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
